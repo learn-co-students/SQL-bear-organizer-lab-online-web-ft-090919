c4757eae9d1ded862ee9a4e8a9cbc8c27319ef0d
eclecticcoding <chuck@eclecticsaddlebag.com>
2019-10-07 16:13:24 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Rowdy", 26, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Mr. Chocolate", 20, "M", "dark brown", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 10, "F", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 17, "M", "blonde", "Nice", 0);
+INSERT INTO bears VALUES (5,"Melissa", 4, "F", "red", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 6, "M", "purple", "mad", 1);
+INSERT INTO bears VALUES (7,"Wendy", 8, "F", "blue", "aggresive", 1);
+INSERT INTO bears VALUES (8, null, 30, "M", "b;ack", "calm", 0);
