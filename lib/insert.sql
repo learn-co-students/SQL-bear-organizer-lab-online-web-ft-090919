24e0a8788b9b82d9a5e8b20740d817d62632f0d0
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-08 17:38:30 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "male", "black", "frisky", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "female", "brown", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "mean", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "female", "black", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "male", "brown", "mean", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "female", "black", "friendly", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 15, "male", "brown", "deadly", "true");
