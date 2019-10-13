63da8d991b9eaeb903c0a463091a40e6325cbb30
Alyssa Yates <bosslys@icloud.com>
2019-10-13 00:33:23 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 37, 'Male', 'Black', 'Well Mannered', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Rowdy', 87, 'Male', 'Black', 'Grumpy ol guy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Tabitha', 22, 'Female', 'Black', 'Hungry', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Sergeant Brown', 55, 'Female', 'Brown', 'Bossy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Melissa', 47, 'Female', 'Black', 'Frisky', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Grinch', 40, 'Male', 'Brown', 'Unbothered', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Wendy', 37, 'Female', 'Brown', 'Sleepy', 'true');
+
+INSERT INTO bears (age, gender, color, temperament, alive) VALUES
+(50, 'Male', 'Brown', 'Aggressive', 'true');
